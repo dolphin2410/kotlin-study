6e600e3f34e8bcfdcf0f7d17803285d877fa67e0
dolphin2410 <80996692+dolphin2410@users.noreply.github.com>
2026-03-19 11:46:24 +0900

    Create 1-print.kt

---
+++ study/1-print.kt
@@ -0,0 +1,11 @@
+package dev.scscmc
+
+fun main() {
+    // print는 그냥 출력
+    print("Hello ")
+    print("World!")
+
+    // println은 출력 후 다음 단락으로 넘어간다
+    println("Hello ")
+    println("World!")
+}
